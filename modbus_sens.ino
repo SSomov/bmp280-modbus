058c41236106cbe0b5ae3c3fcf0efa8080aece59
Stanislav Somov <goofma@rambler.ru>
2019-10-11 17:05:13 +0300

    add modbus

--- modbus_sens.ino
+++ modbus_sens.ino
@@ -1,207 +1,56 @@
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface. The device's I2C address is either 0x76 or 0x77.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
-  See the LICENSE file for details.
- ***************************************************************************/
-
-#include <Wire.h>
-#include <SPI.h>
+//#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
+#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
+#include <ArduinoModbus.h>
+#include <math.h>
 
 Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
-unsigned long delayTime;
-
-#include <Modbusino.h>
-
-/* Initialize the slave with the ID 1 */
-ModbusinoSlave modbusino_slave(1);
-/* Allocate a mapping of 10 values */
-uint16_t tab_reg[10];
-
-    union converter{
-float f_val;
-uint16_t u_val;
-};
-
-union converter a;
-
+long temp = 0;
+long humi = 0;
+long pres = 0;
+long tr = 0;
 void setup() {
-    /*Serial.begin(9600);
-    Serial.println(F("BME280 test"));*/
-
-    modbusino_slave.setup(115200);
+    Serial.begin(57600);
+    //Serial.println(F("BME280 test"));
+    ModbusRTUServer.begin(4, 57600);
+    ModbusRTUServer.configureInputRegisters(0x00, 10);
     if (! bme.begin()) {
         // Serial.println("Could not find a valid BME280 sensor, check wiring!");
         while (1);
     }
-
-  /*  Serial.println("-- Default Test --");
-    Serial.println("normal mode, 16x oversampling for all, filter off,");
-    Serial.println("0.5ms standby period");*/
-    delayTime = 5000;
-    /*
-        // инициализация Timer1
-    cli(); // отключить глобальные прерывания
-    TCCR1A = 0; // установить TCCR1A регистр в 0
-    TCCR1B = 0;
-
-    // включить прерывание Timer1 overflow:
-    TIMSK1 = (1 << TOIE1);
-    // Установить CS10 бит так, чтобы таймер работал при тактовой частоте:
-    TCCR1B |= (1 << CS10);
-    TCCR1B |= (1 << CS12);
-
-    sei();  // включить глобальные прерывания
-    */
-    
-    // For more details on the following scenarious, see chapter
-    // 3.5 "Recommended modes of operation" in the datasheet
-    
-/*
-    // weather monitoring
-    Serial.println("-- Weather Station Scenario --");
-    Serial.println("forced mode, 1x temperature / 1x humidity / 1x pressure oversampling,");
-    Serial.println("filter off");
-    bme.setSampling(Adafruit_BME280::MODE_FORCED,
-                    Adafruit_BME280::SAMPLING_X1, // temperature
-                    Adafruit_BME280::SAMPLING_X1, // pressure
-                    Adafruit_BME280::SAMPLING_X1, // humidity
-                    Adafruit_BME280::FILTER_OFF   );
-                      
-    // suggested rate is 1/60Hz (1m)
-    delayTime = 60000; // in milliseconds
-*/
-
-/*    
-    // humidity sensing
-    Serial.println("-- Humidity Sensing Scenario --");
-    Serial.println("forced mode, 1x temperature / 1x humidity / 0x pressure oversampling");
-    Serial.println("= pressure off, filter off");
-    bme.setSampling(Adafruit_BME280::MODE_FORCED,
-                    Adafruit_BME280::SAMPLING_X1,   // temperature
-                    Adafruit_BME280::SAMPLING_NONE, // pressure
-                    Adafruit_BME280::SAMPLING_X1,   // humidity
-                    Adafruit_BME280::FILTER_OFF );
-                      
-    // suggested rate is 1Hz (1s)
-    delayTime = 1000;  // in milliseconds
-*/
-
-/*    
-    // indoor navigation
-    Serial.println("-- Indoor Navigation Scenario --");
-    Serial.println("normal mode, 16x pressure / 2x temperature / 1x humidity oversampling,");
-    Serial.println("0.5ms standby period, filter 16x");
-    bme.setSampling(Adafruit_BME280::MODE_NORMAL,
-                    Adafruit_BME280::SAMPLING_X2,  // temperature
-                    Adafruit_BME280::SAMPLING_X16, // pressure
-                    Adafruit_BME280::SAMPLING_X1,  // humidity
-                    Adafruit_BME280::FILTER_X16,
-                    Adafruit_BME280::STANDBY_MS_0_5 );
-    
-    // suggested rate is 25Hz
-    // 1 + (2 * T_ovs) + (2 * P_ovs + 0.5) + (2 * H_ovs + 0.5)
-    // T_ovs = 2
-    // P_ovs = 16
-    // H_ovs = 1
-    // = 40ms (25Hz)
-    // with standby time that should really be 24.16913... Hz
-    delayTime = 41;
-    */
-    
-    /*
-    // gaming
-    Serial.println("-- Gaming Scenario --");
-    Serial.println("normal mode, 4x pressure / 1x temperature / 0x humidity oversampling,");
-    Serial.println("= humidity off, 0.5ms standby period, filter 16x");
-    bme.setSampling(Adafruit_BME280::MODE_NORMAL,
-                    Adafruit_BME280::SAMPLING_X1,   // temperature
-                    Adafruit_BME280::SAMPLING_X4,   // pressure
-                    Adafruit_BME280::SAMPLING_NONE, // humidity
-                    Adafruit_BME280::FILTER_X16,
-                    Adafruit_BME280::STANDBY_MS_0_5 );
-                      
-    // Suggested rate is 83Hz
-    // 1 + (2 * T_ovs) + (2 * P_ovs + 0.5)
-    // T_ovs = 1
-    // P_ovs = 4
-    // = 11.5ms + 0.5ms standby
-    delayTime = 12;
-*/
-
-    //Serial.println();
 }
 
 
-void loop() {
-    // Only needed in forced mode! In normal mode, you can remove the next line.
-    bme.takeForcedMeasurement(); // has no effect in normal mode
+void loop() {    
+    ModbusRTUServer.poll();
+    temp = bme.readTemperature()*10;
+    pres = (long)round(bme.readPressure()*7.50062/100.0);
+    humi = bme.readHumidity()*10;
+    ModbusRTUServer.inputRegisterWrite(0  , 1);
+    ModbusRTUServer.inputRegisterWrite(1  , temp);
+    ModbusRTUServer.inputRegisterWrite(2  , 1);
+    ModbusRTUServer.inputRegisterWrite(3  , humi);
+    //tr = long(dewPoint(temp/ 10.0, humi/ 10.0)*10);
+    ModbusRTUServer.inputRegisterWrite(4  , tr);
+    ModbusRTUServer.inputRegisterWrite(5  , 1);
+    ModbusRTUServer.inputRegisterWrite(6  , pres);
     
-    printValues();
-    //delay(delayTime);
 }
 
-
-void printValues() {
-  /*  Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" *C");
-    Serial.println(int(bme.readTemperature()+0.51));
-    Serial.println(int(bme.readTemperature()+0.51), HEX);
-    Serial.print(String(int(bme.readTemperature()+0.51), HEX));
-    
-    Serial.print("Pressure = ");
-
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-
-    Serial.println();*/
-
-    a.f_val = bme.readTemperature();
-
-        /* Initialize the first register to have a value to read */
-    tab_reg[0] = a.u_val;
-    /* Launch Modbus slave loop with:
-       - pointer to the mapping
-       - max values of mapping */
-    modbusino_slave.loop(tab_reg, 10);
-}
-
-
-ISR(TIMER1_OVF_vect)
+double dewPoint(float celsius, float humidity)
 {
-    a.f_val = bme.readTemperature();
-
-        /* Initialize the first register to have a value to read */
-    tab_reg[0] = a.u_val;
+  // (1) Saturation Vapor Pressure = ESGG(T)
+  double RATIO = 373.15 / (273.15 + celsius);
+  double RHS = -7.90298 * (RATIO - 1);
+  RHS += 5.02808 * log10(RATIO);
+  RHS += -1.3816e-7 * (pow(10, (11.344 * (1 - 1/RATIO ))) - 1) ;
+  RHS += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1) ;
+  RHS += log10(1013.246);
+  // factor -3 is to adjust units - Vapor Pressure SVP * humidity
+  double VP = pow(10, RHS - 3) * humidity;
+  // (2) DEWPOINT = F(Vapor Pressure)
+  double T = log(VP/0.61078);   // temp var
+  return (241.88 * T) / (17.558 - T);
 }
